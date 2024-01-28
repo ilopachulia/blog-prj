8cd2e0c15fbb6fd641add00a8d013ea6c5c7b8f8
Ilia Pachulia <ilia.pachulia@qauntori.com>
2024-01-28 13:58:06 +0400

    added tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
-
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/account');
+        console.log(response);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
-     
+      <h1 className='text-3xl font-bold text-red-300 underline'>
+        Hello world!
+      </h1>{' '}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
